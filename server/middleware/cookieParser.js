400f43e2beb95453c8bcdb834c67a1f85c18b50d
mnutt346 <mnutt346@gmail.com>
2018-12-08 12:36:24 -0600

    (feat) Pass first test cookie parser

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,19 @@
+const cookie = require('cookie');
+
 const parseCookies = (req, res, next) => {
+    if (req.headers.cookie === undefined) {
+
+        return next();
+
+    } else if (req.headers.cookie) {
+
+        let cookies = cookie.parse(req.headers.cookie);
+        req.cookies = cookies;
+
+        return next();
+    }
+
+
 };
 
 module.exports = parseCookies;
